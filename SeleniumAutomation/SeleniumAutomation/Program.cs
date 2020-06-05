a37b8207b1b0c5b8a99925f1c18b4f24dbb788d9
rrsimt.rahul@gmail.com <HACK@1903>
2020-06-05 15:09:05 +0400

    Code changes for taking screenshpt using Selenium Automation

---
+++ SeleniumAutomation/SeleniumAutomation/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+
+namespace SeleniumAutomation
+{
+    class Program
+    {
+        static string url = @"https://www.google.com";
+        static string imgPath = @"D:\SeleniumTestingScreenshot.jpg";
+        static void Main(string[] args)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");//Comment if we want to see the window. 
+            var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
+            driver.Navigate().GoToUrl(url);
+            var screenshot = (driver as ITakesScreenshot).GetScreenshot();
+            screenshot.SaveAsFile(imgPath, ImageFormat.Jpeg);
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
